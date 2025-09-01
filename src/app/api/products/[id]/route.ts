a02e88514f867f28f93e63fb80817911ebf2b93d
Pranoy Biswas <impranoybiswas@gmail.com>
2025-09-02 00:20:22 +0600

    Promise and Awit in route

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -4,10 +4,10 @@ import { getProductsCollection } from "../../collection";
 
 
 //__________GET Single Data
-export async function GET(request: NextRequest, context: { params: Awaited<{ id: string }> }) {
+export async function GET(request: NextRequest, context: { params: Promise<{ id: string }> }) {
   try {
     const collection = await getProductsCollection();
-    const { id } = context.params;
+    const { id } = await context.params;
     const product = await collection.findOne({ _id: new ObjectId(id) });
     return new NextResponse(JSON.stringify(product), { status: 200 });
   } catch (error) {
@@ -17,10 +17,10 @@ export async function GET(request: NextRequest, context: { params: Awaited<{ id:
 }
 
 //__________PATCH Data
-export async function PATCH(request: NextRequest, context: { params: Awaited<{ id: string }> }) {
+export async function PATCH(request: NextRequest, context: { params: Promise<{ id: string }> }) {
   try {
     const collection = await getProductsCollection();
-    const { id } = context.params;
+    const { id } = await context.params;
     const data = await request.json();
     const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: data });
     return new NextResponse(JSON.stringify(result), { status: 200 });
@@ -31,10 +31,10 @@ export async function PATCH(request: NextRequest, context: { params: Awaited<{ i
 }
 
 //__________DELETE Data
-export async function DELETE(request: NextRequest, context: { params: Awaited<{ id: string }> }) {
+export async function DELETE(request: NextRequest, context: { params: Promise<{ id: string }> }) {
   try {
     const collection = await getProductsCollection();
-    const {id} = context.params;
+    const {id} = await context.params;
     const result = await collection.deleteOne({ _id: new ObjectId(id) });
     return new NextResponse(JSON.stringify(result), { status: 200 });
   } catch (error) {
